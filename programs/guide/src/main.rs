cdf5b6d50b91e881b89738a57db14e1ca5006832
mirko-geno <mirkogenovesi24@gmail.com>
2025-07-20 18:54:16 -0300

    model definition

---
+++ programs/guide/src/main.rs
@@ -0,0 +1,15 @@
+mod model;
+
+use crate::model::ModelConfig;
+use burn::backend::Cuda;
+
+fn main() {
+    type Backend = Cuda<f32, i32>;
+
+    let device = Default::default();
+    let model = ModelConfig::new(10, 512).init::<Backend>(&device);
+
+    println!("{model}");
+
+
+}
