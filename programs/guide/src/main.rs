e1153a2804e3484d6a07b3f36ba21a2e0f24f6e3
mirko-geno <mirkogenovesi24@gmail.com>
2025-07-20 22:44:31 -0300

    inference

--- programs/guide/src/main.rs
+++ programs/guide/src/main.rs
@@ -1,14 +1,14 @@
 mod model;
 mod data;
 mod training;
+mod inference;
 
 use crate::{
     model::ModelConfig,
-    training::{TrainingConfig, train}
+    training::TrainingConfig
 };
 use burn::{
-    backend::{Autodiff, Cuda},
-    optim::AdamConfig,
+    backend::{Autodiff, Cuda}, data::dataset::Dataset, optim::AdamConfig
 };
 
 fn main() {
@@ -18,9 +18,15 @@ fn main() {
     let device = burn::backend::cuda::CudaDevice::default();
     let artifact_dir = "/tmp/guide";
 
-    train::<AutodiffBackend>(
+    crate::training::train::<AutodiffBackend>(
         artifact_dir,
         TrainingConfig::new(ModelConfig::new(10, 512), AdamConfig::new()),
         device.clone(),
+    );
+
+    crate::inference::infer::<Backend>(
+        artifact_dir,
+        device,
+        burn::data::dataset::vision::MnistDataset::test().get(42).unwrap()
     );
 }
